faf4c7f23b8a0e858dc8d5c977f9b0f65b596055
yasasvif5 <23wh1a05f5@bvrithyderabad.edu.in>
2025-06-23 14:27:39 +0530

    Initial commit

---
+++ config/db.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  HOST: "localhost",
+  USER: "root",
+  PASSWORD: "1234",  // updated password
+  DB: "event_db",    // database name
+  dialect: "mysql"   // database dialect (MySQL in this case)
+};
+
